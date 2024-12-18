f70522ec53454021b5fb676da0df505be01a3e0e
DennyZhen1312 <155478572+DennyZhen1312@users.noreply.github.com>
2024-12-18 08:11:07 -0800

    Delete Class 6(NodeJS)/Week 4 directory

--- Class 6(NodeJS)/Week 4/src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { apiRouter } from "./routes/api";
-
-export const app = express();
-
-app.use(express.json());
-
-app.use("/api/v1", apiRouter);
--- Class 6(NodeJS)/Week 4/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app";
-import { HOST, PORT } from "./env";
-
-
-app.listen(PORT, () => {
-  console.log(`[server]: Listening at http://${HOST}:${PORT}`);
-});
