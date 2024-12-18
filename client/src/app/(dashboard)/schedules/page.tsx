449fb7fb72b51b4cefb0dfac1748edd663ebbbd2
Bart Kang <bart.kang.0719@gmail.com>
2024-12-18 13:57:18 -0800

    fix bugs

--- client/src/app/(dashboard)/schedules/page.tsx
+++ client/src/app/(dashboard)/schedules/page.tsx
@@ -1,30 +1,26 @@
 "use client";
 
-
 // pages/scheduler.tsx
-import React, { useEffect } from "react";
+import { registerLicense } from "@syncfusion/ej2-base";
 import "@syncfusion/ej2-base/styles/material.css";
 import "@syncfusion/ej2-buttons/styles/material.css";
 import "@syncfusion/ej2-calendars/styles/material.css";
 import "@syncfusion/ej2-dropdowns/styles/material.css";
 import "@syncfusion/ej2-inputs/styles/material.css";
-import "@syncfusion/ej2-splitbuttons/styles/material.css";
 import "@syncfusion/ej2-lists/styles/material.css";
 import "@syncfusion/ej2-navigations/styles/material.css";
 import "@syncfusion/ej2-popups/styles/material.css";
-import "@syncfusion/ej2-schedule/styles/material.css";
 import {
-  ScheduleComponent,
-  Day,
-  Week,
-  WorkWeek,
-  Month,
-  Agenda,
   Inject,
+  ScheduleComponent,
+  Week
 } from "@syncfusion/ej2-react-schedule";
-import { registerLicense } from '@syncfusion/ej2-base';
+import "@syncfusion/ej2-schedule/styles/material.css";
+import "@syncfusion/ej2-splitbuttons/styles/material.css";
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpR2ZGfV5ycEVPal9RTnNfUiweQnxTdEFiW35WcH1WRmVcU0dwVw==');
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpR2ZGfV5ycEVPal9RTnNfUiweQnxTdEFiW35WcH1WRmVcU0dwVw=="
+);
 
 const Scheduler = () => {
   const eventData = [
@@ -32,14 +28,14 @@ const Scheduler = () => {
       Id: 1,
       Subject: "Conference",
       StartTime: new Date(2024, 11, 17, 10, 0),
-      EndTime: new Date(2024, 11, 17, 12, 30),
+      EndTime: new Date(2024, 11, 17, 12, 30)
     },
     {
       Id: 2,
       Subject: "Meeting",
       StartTime: new Date(2024, 11, 18, 9, 0),
-      EndTime: new Date(2024, 11, 18, 11, 0),
-    },
+      EndTime: new Date(2024, 11, 18, 11, 0)
+    }
   ];
 
   return (
@@ -53,16 +49,13 @@ const Scheduler = () => {
         //     title: null
         //   }
         // } }}
-        popupOpen={(args) => console.log(args)
-        }
+        popupOpen={(args) => console.log(args)}
         timeScale={{ enable: false }}
-        
       >
-        <Inject services={Week} />
+        <Inject services={[Week]} />
       </ScheduleComponent>
     </div>
   );
 };
-
 
 export default Scheduler;
---
+++ server/prisma/migrations/20241218214613_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Employee" ADD COLUMN     "is_availability_requested" BOOLEAN NOT NULL DEFAULT false;
