7d7ad66ce2c2f94a946151944584e651fb7773dd
Bart Kang <bart.kang.0719@gmail.com>
2024-12-18 22:37:40 -0800

    change directory

---
+++ client/src/actions/actions/notifications.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+
+type Notification = {
+  id: number;
+  message: string;
+};
+
+// export const getNotifications = (): Notification[] => {
+//   return [{ id: 1, message: "Please submit the weekly availability " }];
+// };
+
+export const getNotifications = async (): Promise<Notification[]> => {
+  const { getToken } = await auth();
+  const token = await getToken();
+
+  const response = await fetch("http://localhost:4000/api/notifications", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error("Something went wrong while getting notifications");
+  }
+
+  const notifications: Notification[] = await response.json();
+
+  return notifications;
+};
