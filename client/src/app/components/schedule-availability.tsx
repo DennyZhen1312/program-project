491f97dc05cfa84e4546345dec217e2385856723
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2024-12-18 16:19:42 -0800

    Breaking in different files to maximize readability

--- client/src/app/components/schedule-availability.tsx
+++ client/src/app/components/schedule-availability.tsx
@@ -14,8 +14,8 @@ import {
   ViewDirective,
   ViewsDirective,
 } from "@syncfusion/ej2-react-schedule";
-import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
 import { AvailabilityType } from "@/types/types";
+import { editorTemplate } from "./editor-template";
 
 registerLicense(
   "Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpR2ZGfV5ycEVPal9RTnNfUiweQnxTdEFiW35WcH1WRmVcU0dwVw=="
@@ -39,43 +39,6 @@ const fieldsData = {
     title: "Employee Name",
     default: "Employee Name",
   },
-};
-
-const editorTemplate = (props: object): JSX.Element => {
-  return props !== undefined ? (
-    <table className="custom-event-editor w-full px-6">
-      <tbody>
-        <tr>
-          <td className="e-textlabel">From</td>
-          <td colSpan={4}>
-            <DateTimePickerComponent
-              format="dd/MM/yy hh:mm a"
-              id="StartTime"
-              data-name="StartTime"
-              value={
-                new Date((props as any).startTime || (props as any).StartTime)
-              }
-              className="e-field"
-            ></DateTimePickerComponent>
-          </td>
-        </tr>
-        <tr>
-          <td className="e-textlabel">To</td>
-          <td colSpan={4}>
-            <DateTimePickerComponent
-              format="dd/MM/yy hh:mm a"
-              id="EndTime"
-              data-name="EndTime"
-              value={new Date((props as any).endTime || (props as any).EndTime)}
-              className="e-field"
-            ></DateTimePickerComponent>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  ) : (
-    <div></div>
-  );
 };
 
 const ScheduleAvailability = () => {
